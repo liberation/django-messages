bf0afc2256c798a353a9af385bf26e66adef419e
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-10-12 19:33:30 +0200

    added tests for manager

--- django_messages/tests/__init__.py
+++ django_messages/tests/__init__.py
@@ -1,2 +1,3 @@
 from test_models import *
+from test_manager import *
 from test_command_clean_deleted_messages import *
--- django_messages/tests/utils.py
+++ django_messages/tests/utils.py
@@ -5,13 +5,14 @@ from django_messages.models import Message
 
 class DjangoMessagesTestCase(TestCase):
    
-    def send_message(self, sender, recipient, parent=None):
+    def send_message(self, sender, recipient, parent=None, conversation=None):
         msg = Message(
             sender=sender,
             recipient=recipient,
             subject='Subject Text',
             body='Body Text',
             parent_msg=parent,
+            conversation=conversation,
         )
         msg.save()
         return msg
