330fc420b6fdfc648b325257fd907824aeee3adb
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-10-12 18:18:20 +0200

    added tests for models

--- django_messages/tests/test_models.py
+++ django_messages/tests/test_models.py
@@ -1,44 +1,108 @@
-import datetime
+from datetime import datetime
+
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django_messages.models import Message
+from django_messages.models import Message, inbox_count_for
 
-class SendTestCase(TestCase):
+class DjangoMessagesTestCase(TestCase):
+   
+    def send_message(self, sender, recipient, parent=None):
+        msg = Message(
+            sender=sender,
+            recipient=recipient,
+            subject='Subject Text',
+            body='Body Text',
+            parent_msg=parent,
+        )
+        msg.save()
+        return msg
+
+
+class MessageTests(DjangoMessagesTestCase):
+
     def setUp(self):
         self.user1 = User.objects.create_user('user1', 'user1@example.com', '123456')
         self.user2 = User.objects.create_user('user2', 'user2@example.com', '123456')
+        self.user3 = User.objects.create_user('user3', 'user3@example.com', '123456')
+
+    def test_user_sent_messages(self):
+        """Tests that a message sent by a user is available in ``sent_messages``
+        property of the ``User`` model and only to the user who sent the message"""
+        msg = self.send_message(self.user1, self.user2)
         
-    def test_user1_send_a_message_to_user2(self):
-        # simulates a message sent by user1 to user2
-        msg1 = Message(sender=self.user1, recipient=self.user2, subject='Subject Text', body='Body Text')
-        msg1.save()
+        self.assertEquals(self.user1.sent_messages.all().count(), 1)
+        self.assertEquals(self.user2.sent_messages.all().count(), 0)
+        self.assertEquals(self.user3.sent_messages.all().count(), 0)
         
-        self.assertEquals(self.user1.sent_messages.count(), 1)
-        self.assertEquals(self.user1.received_messages.count(), 0)
-        self.assertEquals(self.user2.received_messages.count(), 1)
-        self.assertEquals(self.user2.sent_messages.count(), 0)
+    def test_user_received_messages(self):
+        """Tests that a message sent to a user is available in ``received_messages``
+        property of the ``User`` model and only to the user the message was sent"""
+        msg = self.send_message(self.user1, self.user2)
         
-class DeleteTestCase(TestCase):
-    def setUp(self):
-        self.user1 = User.objects.create_user('user3', 'user3@example.com', '123456')
-        self.user2 = User.objects.create_user('user4', 'user4@example.com', '123456')
-        self.msg1 = Message(sender=self.user1, recipient=self.user2, subject='Subject Text 1', body='Body Text 1')
-        self.msg2 = Message(sender=self.user1, recipient=self.user2, subject='Subject Text 2', body='Body Text 2')
-        self.msg1.sender_deleted_at = datetime.datetime.now()
-        self.msg2.recipient_deleted_at = datetime.datetime.now()
-        self.msg1.save()
-        self.msg2.save()
-                
-    def testBasic(self):
-        self.assertEquals(Message.objects.outbox_for(self.user1).count(), 1)
-        self.assertEquals(Message.objects.outbox_for(self.user1)[0].subject, 'Subject Text 2')
-        self.assertEquals(Message.objects.inbox_for(self.user2).count(),1)
-        self.assertEquals(Message.objects.inbox_for(self.user2)[0].subject, 'Subject Text 1')
-        #undelete
-        self.msg1.sender_deleted_at = None
-        self.msg2.recipient_deleted_at = None
-        self.msg1.save()
-        self.msg2.save()
-        self.assertEquals(Message.objects.outbox_for(self.user1).count(), 2)
-        self.assertEquals(Message.objects.inbox_for(self.user2).count(),2)
+        self.assertEquals(self.user1.received_messages.all().count(), 0)
+        self.assertEquals(self.user2.received_messages.all().count(), 1)
+        self.assertEquals(self.user3.received_messages.all().count(), 0)
+
+    def test_message_next_messages(self):
+        """Tests that a message sent as children of a message is accessible as an
+        item of the ``next_messages`` property of the parent message"""
         
+        parent = self.send_message(self.user1, self.user2)
+        
+        self.assertEquals(parent.next_messages.all().count(), 0)
+        
+        msg = self.send_message(self.user1, self.user2, parent=parent)
+        
+        self.assertEquals(parent.next_messages.all().count(), 1)
+        self.assertEquals(parent.next_messages.all()[0].pk, msg.pk)
+    
+    def test_message_is_new(self):
+        """If the message is not read is new"""
+        msg = self.send_message(self.user1, self.user2)
+        
+        self.assertTrue(msg.new())
+        
+    def test_message_is_not_new(self):
+        """If the message ``read_at`` property is set to something, the message
+        is considered read"""
+        msg = self.send_message(self.user1, self.user2)
+        msg.read_at = datetime.now()
+        
+        self.assertFalse(msg.new())
+
+    def test_message_is_replied(self):
+        """If the message is ``replied_at`` property is set to something, the message
+        is considered read"""
+        msg = self.send_message(self.user1, self.user2)
+        msg.replied_at = datetime.now()
+        self.assertTrue(msg.replied())
+        
+    def test_message_sent_is_set(self):
+        """Once a message is sent, ``sent_at`` property should be set"""
+        msg = self.send_message(self.user1, self.user2)
+        self.assertIsNotNone(msg.sent_at)
+        
+    def test_user_inbox_is_initialy_empty(self):
+        self.assertEquals(inbox_count_for(self.user1), 0)
+        
+    def test_user_inbox(self):
+        """Tests that only the user a message was sent has an inbox count non null"""
+        msg = self.send_message(self.user1, self.user2)
+        
+        self.assertEquals(inbox_count_for(self.user1), 0)  # sender
+        self.assertEquals(inbox_count_for(self.user2), 1)  # recipient
+        self.assertEquals(inbox_count_for(self.user3), 0)  # random user
+        
+    def test_user_inbox_only_unread_message(self):
+        msg = self.send_message(self.user1, self.user2)
+        msg.read_at = datetime.now()  # set message read
+        msg.save()
+        
+        self.assertEquals(inbox_count_for(self.user2), 0)
+
+    def test_user_inbox_not_deleted_messages(self):
+        msg = self.send_message(self.user1, self.user2)
+        msg.recipient_deleted_at = datetime.now()  # recipient deleted conversation thus the message
+        msg.save()
+        
+        self.assertEquals(inbox_count_for(self.user2), 0)
